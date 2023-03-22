15aae09e015f540b9b9e254a81cff8abe3c9401b
Squiro <alesio_sinopoli@hotmail.com>
2023-03-22 20:32:34 -0300

    Update about-me text

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -13,7 +13,7 @@ export class AboutMeComponent extends BaseComponent {
         this.addComponentString(
             [
                 'Basic info:', 
-                '(Very soon to be a) Computer Science Engineer. Currently working as a Full Stack Web Developer, but not all my projects revolve around that.',
+                'Computer Science Engineer. Currently working as a Full Stack Web Developer, but not all my projects revolve around that.',
                 `I standout for my ability to solve problems. I'm constantly in the pursuit of new knowledge and techniques that I can acquire, learn, and put to test on my working field.`,
                 'I make all my solutions, projects and work focusing on quality, always placing myself on the side of the end user.',
                 'Relevant links:',
